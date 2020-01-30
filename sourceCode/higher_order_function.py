16b3467b87b32d0f407d177c0d3616673a581116
Hoen <hoenwithsalon@gmail.com>
2020-01-30 23:33:59 +0300

    higher-order functions

---
+++ sourceCode/higher_order_function.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Higher-order functions
+# What is a Higher-order functions>
+# Since a variable can point to a function and its parameters can receive variables, then a function can receive another function as a parameter.
+# this kind of function is called a higher-order function
+
+# def add(x, y, f):
+#     return f(x) + f(y)
+# print(add(-5, 6, abs))
